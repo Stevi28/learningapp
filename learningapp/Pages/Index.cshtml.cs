f29b52bc8c1de9099211b4bf349fb1fbc175167d
Stavroula.Kourou <stavroula1.kourou@StavroulaKourous-MacBook-Air.local>
2025-05-10 22:17:54 +0300

    first commit

---
+++ learningapp/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace learningapp.Pages;
+
+public class IndexModel : PageModel
+{
+     public List<Course> Courses=new List<Course>();
+    private readonly ILogger<IndexModel> _logger;
+    private IConfiguration _configuration;
+    public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration=configuration;
+    }
+}
